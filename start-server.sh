9a4013d1ceb06dfe0f1c4a7db34e59d5a78d0c19
DBKynd <db@dbkynd.com>
2019-08-31 17:50:45 -0600

    download server.properties

--- start-server.sh
+++ start-server.sh
@@ -21,10 +21,27 @@ if [ ! -f $SERVER_DIR/${JAR_NAME}.jar ]; then
   cd ${SERVER_DIR}
   echo "Downloading Minecraft Server 1.14.4..."
   wget -qi ${JAR_NAME} https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar
-  echo "Download Complete"
+  if [ ! -f $SERVER_DIR/${JAR_NAME}.jar ]; then
+    echo "Error downloading server.jar file."
+    sleep infinity
+    exit
+  else
+    echo "Download Complete"
+  fi
 fi
 
-
+# Download default server.properties file if one is not found
+if [ ! -f ${SERVER_DIR}/server.properties ]; then
+  echo "Downloading default server.properties file..."
+  wget -qi ${SERVER_DIR}/server.properties https://raw.githubusercontent.com/dbkynd/docker-minecraft/master/server.properties
+  if [ ! -f ${SERVER_DIR}/server.properties ]; then
+    echo "Error downloading server.properties file."
+    sleep infinity
+    exit
+  else
+    echo "Download Complete"
+  fi
+fi
 
 trap shutdown SIGINT
 
